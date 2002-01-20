f2f3b97cbf7b5e3c44f4be972300c7e704692991
Carl <carl@izap.in>
2002-01-20 02:47:03 +0000

    New 1.8

---
+++ languages/en.php
@@ -0,0 +1,22 @@
+<?php
+/**************************************************
+* PluginLotto.com                                 *
+* Copyrights (c) 2005-2010. iZAP                  *
+* All rights reserved                             *
+***************************************************
+* @author iZAP Team "<support@izap.in>"
+* @link http://www.izap.in/
+* @version {version} $Revision: {revision}
+* Under this agreement, No one has rights to sell this script further.
+* For more information. Contact "Tarun Jangra<tarun@izap.in>"
+* For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
+* Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
+ */
+
+$english = array(
+        'izap_autotags:admin_settings' => 'Do you want to allow users to add new tags ?',
+        'izap_autotags:yes' => 'Yes',
+        'izap_autotags:no' => 'No',
+        'izap_autotags:not_allowed' => 'You are not allowed to enter the new tags.'
+);
+add_translation('en', $english);
---
+++ views/default/settings/izap-autocomplete-tags/edit.php
@@ -0,0 +1,25 @@
+<?php
+/**************************************************
+* PluginLotto.com                                 *
+* Copyrights (c) 2005-2010. iZAP                  *
+* All rights reserved                             *
+***************************************************
+* @author iZAP Team "<support@izap.in>"
+* @link http://www.izap.in/
+* @version 1.0
+* Under this agreement, No one has rights to sell this script further.
+* For more information. Contact "Tarun Jangra<tarun@izap.in>"
+* For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
+* Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
+*/
+echo elgg_echo('izap_autotags:admin_settings');
+$value = $vars['entity']->autocompletetags;
+if(!in_array($value, array('yes', 'no'))) {
+  $value = 'yes';
+}
+?>
+
+<select name="params[autocompletetags]">
+  <option value="no" <?php if($value != 'yes'){ echo "selected"; } ?>><?php echo elgg_echo('izap_autotags:no');?></option>
+  <option value="yes" <?php if($value == 'yes'){ echo "selected"; } ?>><?php echo elgg_echo('izap_autotags:yes');?></option>
+</select>
