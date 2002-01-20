c6e00b27b601b9ef45e99b997bd84baa61a21bc8
Carl <carl@izap.in>
2002-01-20 02:46:42 +0000

    New 1.8

---
+++ start.php
@@ -0,0 +1,44 @@
+<?php
+/**************************************************
+* PluginLotto.com                                 *
+* Copyrights (c) 2005-2010. iZAP                  *
+* All rights reserved                             *
+***************************************************
+* @author iZAP Team "<support@izap.in>"
+* @link http://www.izap.in/
+* @version 1.0
+* Under this agreement, No one has rights to sell this script further.
+* For more information. Contact "Tarun Jangra<tarun@izap.in>"
+* For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
+* Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
+ */
+
+/*
+ * define some globals
+ */
+
+define('GLOBAL_IZAP_AUTOCOMPLEATE_TAGS_PLUGIN', 'izap-autocomplete-tags');
+
+//Hook the plugin with the system
+elgg_register_event_handler('init', 'system', 'izap_autocomplete_tags_init');
+
+
+/**
+ * main init function, that will be hooked
+ */
+function izap_autocomplete_tags_init() {
+  // start plugin
+  izap_plugin_init(GLOBAL_IZAP_AUTOCOMPLEATE_TAGS_PLUGIN);
+}
+
+function autocomplete_tags() {
+  $tags = elgg_get_tags();
+
+  foreach($tags as $key=>$val) {
+    $tag[] = '"' . $val->tag .' ('.$val->total.')'. '"';
+  }
+
+  $string = implode(', ', $tag);
+
+  return $string;
+}
