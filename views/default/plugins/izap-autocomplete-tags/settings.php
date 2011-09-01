9b32eec6d7eec16b58741f767a64305a4f1dabdc
Rahul Sethi <rahul@izap.in>
2011-09-01 13:32:29 +0000

    deprecation removed

--- views/default/plugins/izap-autocomplete-tags/settings.php
+++ views/default/plugins/izap-autocomplete-tags/settings.php
@@ -3,13 +3,14 @@
 * PluginLotto.com                                 *
 * Copyrights (c) 2005-2010. iZAP                  *
 * All rights reserved                             *
+*                                                 *
 ***************************************************
 * @author iZAP Team "<support@izap.in>"
 * @link http://www.izap.in/
 * @version 1.0
 * Under this agreement, No one has rights to sell this script further.
 * For more information. Contact "Tarun Jangra<tarun@izap.in>"
-* For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
+* For discussion about corresponding plugins, visit http://www.pluginlotto.com/forums/
 * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
 */
 echo elgg_echo('izap_autotags:admin_settings');
